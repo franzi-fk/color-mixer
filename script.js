0abea5748705c1cabaf7a0d991ef3d0f306514db
Franziska Kiel <kiel.franziska@outlook.com>
2025-02-04 10:29:04 +0100

    feat: random color button

--- script.js
+++ script.js
@@ -1,23 +1,49 @@
+/*----------------------------------------------------------*/
+/* GLOBAL SCOPE VARIABLES */
+
 const sliders = document.querySelector("#sliders-container");
 const sliderRed = document.querySelector("#slider-red");
 const sliderGreen = document.querySelector("#slider-green");
 const sliderBlue = document.querySelector("#slider-blue");
-const hex = document.querySelector("#hex-value");
-
+const btnRandom = document.querySelector("#btn-random");
 const root = document.documentElement;
 
-sliders.addEventListener("input", function () {
-  updateColorValues();
-  updateHex();
-});
+/*----------------------------------------------------------*/
+/* INIT */
+
+updateColorValues();
+updateHex();
+
+sliders.addEventListener("input", updateColorValues);
+btnRandom.addEventListener("click", applyRandomColor);
+
+/*----------------------------------------------------------*/
+/* FUNCTION DEFINITIONS */
 
 function updateColorValues() {
   root.style.setProperty("--r-value", sliderRed.value);
   root.style.setProperty("--g-value", sliderGreen.value);
   root.style.setProperty("--b-value", sliderBlue.value);
+  updateHex();
+}
+
+function applyRandomColor() {
+  fetch("https://dummy-apis.netlify.app/api/color")
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+    })
+    .then((color) => {
+      sliderRed.value = Number(color.rgb.r);
+      sliderGreen.value = Number(color.rgb.g);
+      sliderBlue.value = Number(color.rgb.b);
+      updateColorValues();
+    });
 }
 
 function updateHex() {
+  const hex = document.querySelector("#hex-value");
   hex.innerText = rgbToHex(
     Number(sliderRed.value),
     Number(sliderGreen.value),
